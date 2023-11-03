587fb7e28008f6af0bf2374c34fea4d04f32b6b3
DevEliasKh <20316165+DevEliasKh@users.noreply.github.com>
2023-11-03 15:12:37 +0330

    make store page load products

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,40 +1,34 @@
 import './Store.scss';
-
+import { useState } from 'react';
 import ProductCard from '../component/ProductCard.tsx';
 import productList from '/src/data/products.json';
 
 export default function Store() {
-  const data = {
-    id: '1C',
-    name: 'Lucaffé Mamma Lucia',
-    imageSrc:
-      'https://www.cremashop.eu/media/cache/gallery_image/content/products/lucaffe/mamma-lucia/364-130ce07a4a9cfa784003abfa605256da.jpg',
-    price: 22.9,
-    description:
-      'Lucaffé Mamma Lucia is ideal for morning coffee moments. This sweet coffee works perfectly in milk coffees, such as in your cappuccino and caffè latte. The coffee gives a rich crema.The blend consists of 40 % Arabica and 60 % Robusta beans. For a quick start in the morning!'
-  };
-  function handleClick() {}
+  const [type, setType] = useState('Coffee');
+  function handleClick(e) {
+    setType(e.target.value);
+  }
 
-  productList.map((p: string) => p.type == 'Coffee' && console.log(p.list));
+  const products = productList.filter((p: string) => p.type == type)[0].list;
+  console.log(products);
   return (
     <div className="container">
       <div className="btns">
-        <button className="btn" onClick={handleClick} value="coffee">
+        <button className="btn" onClick={handleClick} value="Coffee">
           Coffee
         </button>
-        <button className="btn" onClick={handleClick} value="machine">
+        <button className="btn" onClick={handleClick} value="Machine">
           Machines
         </button>
-        <button className="btn" onClick={handleClick} value="accessory">
+        <button className="btn" onClick={handleClick} value="Accessory">
           Accessory
         </button>
       </div>
       <div className="cardList">
         <div className="cards">
-          <ProductCard product={data} productUrl={'/#'} />
-          {/* {#each productList as product}
-        <ProductCard {product} onClick={() => addToCart(product)} productUrl="/product/{type}/{product.id}" />
-      {/each} */}
+          {products.map((product) => (
+            <ProductCard product={product} productUrl={'/#'} />
+          ))}
         </div>
       </div>
     </div>
